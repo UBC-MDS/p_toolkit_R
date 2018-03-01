9d25cca1c2cf1143f59bf32e5c28ee14fbb6579d
vmulholl <vero.mulholland@gmail.com>
2018-03-01 15:08:18 -0800

    Add p_methods.R docsting

---
+++ R/p_methods.R
@@ -0,0 +1,12 @@
+p_methods <- function(data, pv_index, alpha){
+  #
+  ## Returns a summary dataframe with columns for the p-values, adjusted p-values
+  ## for both Bonferroni and Benjamini-Hochberg (BH)
+  #
+  ## Args:
+  #     - data (dataframe): dataframe containing at least a column of p-values to be adjusted
+  #     - pv_index (int): original p-value column index from existing input dataframe
+  #     - alpha (int): define significance level as a value between 0 and 1
+  #
+  
+}
