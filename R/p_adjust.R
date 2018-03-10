d4b52e749833800f643141116a80441bb058f669
Amy Goldlist <amygoldlist@gmail.com>
2018-03-10 09:33:54 -0800

    changes from debugging

--- R/p_adjust.R
+++ R/p_adjust.R
@@ -41,26 +41,30 @@ p_adjust <- function(data, pv_index, method, alpha=0.05){
   require(dplyr)
   if(is.data.frame(data)){
     ## check if all p-values are numeric
-    if(sapply(data[pv_index], is.numeric)){
-      data <- select(data, p_value = c(pv_index))
-      return(data)
-    }else{
+    #if(sapply(data[pv_index], is.numeric)){
+      #data <- select(data, p_value = c(pv_index))
+      #return(data)
+      df <- select(data, p_value = c(pv_index))
+      data <- cbind(data, df)
+      data <- select(data,-one_of(pv_index))
+
+    #}else{
       ## error for non-numeric data frame column
-      stop("Please ensure you have specified the column index of numeric p-values.")
-    }
-    }else if(is.vector(data) | is.matrix(data)){
+     # stop("Please ensure you have specified the column index of numeric p-values.")
+    #}
+    }else if(is.vector(data) ){#| is.matrix(data)){
       if(is.numeric(data)){
-        ## convert matrix or verctor to data frame
+        ## convert matrix or vector to data frame
         data <- data.frame(p_value = data)
-        return(data)
+        #return(data)
       }else{
         ## error for non-numeric vector or matrix
         stop("Please ensure you input a vector or matrix of numeric p-values.")
       }
-      }else{
+    }else{
         ## error for not being a data frame, matrix or vector
         stop("Please enter a data frame, matrix or vector of numeric p-values.")
-      }
+    }
 
   ### set the size of the data
   m = nrow(data)
@@ -72,16 +76,28 @@ p_adjust <- function(data, pv_index, method, alpha=0.05){
       bonf_pvalue = ifelse(p_value*m<1,p_value*m,1),
       # bonf_significant = ifelse(p_value<= bonf_val,TRUE,FALSE),
       rank = rank(p_value, ties.method= "min"),
-      bh_pvalue = p_value*rank/m
+      bh_pvalue = p_value*m/rank
     ) %>%
     select(-rank)
 
-  if(method == 'bh' | method == 'fdr'){
-    return(data %>% select(p_value,bh_pvalue))
-  }else if(method == 'bonf' | method == 'bonferroni'){
-    return(data %>% select(p_value,bonf_pvalue))
+  if(method == 'bh' | method == 'fdr' |method == "BH"){
+    return(data %>% select(-bonf_pvalue))
+  }else if(method == 'bonf' | method == 'bonferroni'| method == "Bonferroni"){
+    return(data %>% select(-bh_pvalue))
   }else{
     return(sprintf("Method should be set as 'bonf' or 'bh' corrections"))
   }
 
 }
+
+#p_adjust(data = data.frame(test = c("test 1"),p=c(0.07)),pv_index ="p", method = "bonferroni")
+
+#p_adjust(data=c(.07,.14), method="bh")
+#p_adjust(data = c(0.07,0.2), method = "BH")
+#testA <- data.frame(test = c("test 1"),p=c(0.07))
+#testA
+#p_adjust(data =testA ,pv_index ="p", method = "bh")
+
+#toy_df <- data.frame(test= c("test1", "test2", "moretest", "test 4"), p = c(.05,.5,.0001, .00000001))
+
+#p_adjust(toy_df,2, method = "bh")
