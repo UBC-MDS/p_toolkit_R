1e6c5b128f13ae058b521639d24ca0092f52b79a
Amy Goldlist <amygoldlist@gmail.com>
2018-03-10 16:45:04 -0800

    created vignette

---
+++ R/ptoolkit-package.r
@@ -0,0 +1,11 @@
+#' ptoolkit.
+#'
+#'p_toolkit is a package designed to help adjust and visualize p-values when using multiple comparisons.
+#'As computing power has become powerful enough to run hundreds or even thousands of statistical tests,
+#'it is important to look at small p-values and try to understand whether the result is small simply by chance,
+#'or whether it truly is significant.
+#'
+#'
+#' @name ptoolkit
+#' @docType package
+NULL
